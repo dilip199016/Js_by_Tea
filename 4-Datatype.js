3af0c94781c76a21ef69a8e7a34146eb2dd31195
Dilip Varma <vddilipvarma@gmail.com>
2023-09-09 16:31:20 +0530

    Datatype

--- 4-Datatype.js
+++ 4-Datatype.js
@@ -1,10 +1,35 @@
 let score="33";
-console.log(typeof score);
-console.log(typeof(score));
+console.log("typeof=",typeof score);
+console.log("Second Typeof=",typeof(score));
+let score1="35abc";
+
+
 let valueNumber=Number(score);
+
 console.log(typeof valueNumber);
+console.log("value",valueNumber);
+console.log("value=",typeof valueNumber);
+console.log("Value=",valueNumber);
 
-// console.log(typeof valueNumber);
-// console.log(valueNumber);
+let valueNumber1=Number(score1);
+
+console.log("VanueNumber1=",typeof valueNumber1);
+console.log("valueNumber1=",valueNumber1);
+
+let score2=undefined;
+console.log(typeof score2);
+console.log(score2);
 
 
+console.log("Checking Boolean Value");
+
+let isBoolean=true;
+console.log(typeof isBoolean);
+let valueNumber2=Number(isBoolean);
+console.log(typeof valueNumber2);
+console.log(valueNumber2);
+
+
+let isboolean=1;
+let booleanisLoggerin=Boolean(isboolean);
+console.log(booleanisLoggerin);
