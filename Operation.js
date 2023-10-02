6be38c672b2a8908ef46e87b7f808302b96e1fb4
Dilip Varma <vddilipvarma@gmail.com>
2023-10-02 15:03:30 +0530

    10-Stack&Heap.html

---
+++ Operation.js
@@ -0,0 +1,26 @@
+let value=3;
+let newValue=-3;
+console.log(newValue);
+
+console.log(35-25);
+console.log(35+79);
+console.log(25*5);
+console.log(2**3);
+console.log(25/5);
+console.log(25%4);
+
+
+let str1="Hello"
+str2=" Dilip"
+let str3=str1+str2;
+console.log(str3);
+
+
+
+console.log("Line Number 20 to 23 all operation");
+console.log("1"+5);;
+console.log(55+"96");
+console.log("25"-10);
+
+console.log("Line Number 26 Output");
+console.log(1+2+ "5");
